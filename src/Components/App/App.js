b89d8f2c18122e4c66485736af0c3f006b8a8dc3
BlakeleyKilgore <bkilgore@digium.com>
2018-06-20 16:24:13 -0500

    Change the Name of a Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -36,6 +37,12 @@ class App extends Component {
     }
   }
 
+  updatePlaylistName(name) {
+    this.setState({
+      playlistName: name
+    })
+  }
+
   render() {
     return (
       <div>
@@ -44,7 +51,7 @@ class App extends Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,11 +3,21 @@ import TrackList from '../TrackList/TrackList';
 import './Playlist.css';
 
 class Playlist extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(event) {
+      this.props.onNameChange(event.target.value);
+    }
+
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={'New Playlist'}/>
-        <TrackList tracks={this.props.playlistTrack} onRemove={this.removeTrack} isRemoval={true}/>
+        <input defaultValue={this.props.name}  onChange={this.handleNameChange} />
+        <TrackList tracks={this.props.playlistTrack} onRemove={this.removeTrack} isRemoval={true} />
         <a className="Playlist-save">SAVE TO SPOTIFY</a>
       </div>
     )
